e913d28a5c9fc35275010bf9e5e2fa0678906c0b
jogyein <wsw8811@naver.com>
2023-09-04 16:36:06 +0900

    EBookService 추가

--- src/main/java/com/woori/bookspring/service/EBookService.java
+++ src/main/java/com/woori/bookspring/service/EBookService.java
@@ -1,8 +1,37 @@
 package com.woori.bookspring.service;
 
+import com.woori.bookspring.entity.ebook.EBook;
+import com.woori.bookspring.repository.EBookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
 @Service
 public class EBookService {
-    //CRUD
+    //1234
+    private final EBookRepository eBookRepository;
+
+    public void createEBook(EBook eBook) { //e북 생성
+        eBookRepository.save(eBook);
+    }
+
+    public EBook getEBook(Long id) { //e북 조회, 검색
+        return eBookRepository.findById(id).get();
+    }
+
+    public void deleteEBook(Long id) { //e북 삭제
+        eBookRepository.deleteById(id);
+    }
+
+    public void updateEBook(EBook eBook) { //e북 수정
+        eBookRepository.save(eBook);
+    }
+
+    public List<EBook> getEBookList() { //e북 리스트,목록
+        return eBookRepository.findAll();
+    }
 }
