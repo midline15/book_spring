919133691e0ba5bb7a09a8c186517a0fb4ea282d
qteb1004 <qteb@naver.com>
2023-09-04 16:44:58 +0900

    BookService  구현

--- src/main/java/com/woori/bookspring/service/BookService.java
+++ src/main/java/com/woori/bookspring/service/BookService.java
@@ -1,8 +1,34 @@
 package com.woori.bookspring.service;
 
+import com.woori.bookspring.entity.Book;
+import com.woori.bookspring.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
-    //CRUD
+    private final BookRepository bookRepository;
+
+    public void createBook(Book book){
+        bookRepository.save(book);
+    }
+
+    public Book getBook(Long bookId){
+        return bookRepository.findById(bookId).get();
+    }
+
+    public void updateBook(Book book){
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Long bookId){
+        bookRepository.findById(bookId);
+    }
+
+    public List<Book> bookList(){
+        return bookRepository.findAll();
+    }
 }
