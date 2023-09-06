0806e2a8504b94465ec04184557939d7fab1bbde
Chunba2 <jmkasdfg@naver.com>
2023-09-06 12:33:20 +0900

    Service 구현

--- src/main/java/com/woori/bookspring/service/AnswerService.java
+++ src/main/java/com/woori/bookspring/service/AnswerService.java
@@ -1,8 +1,37 @@
 package com.woori.bookspring.service;
 
+import com.woori.bookspring.entity.qna.Answer;
+import com.woori.bookspring.repository.AnswerRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AnswerService {
-    //CRUD
+
+    private final AnswerRepository answerRepository;
+
+    //답변 작성
+    @Transactional
+    public void createAnswer(Answer answer) {
+        answerRepository.save(answer);
+    }
+
+    //답변 조회
+    public void getAnswer(Long id) {
+        answerRepository.findById(id).get();
+    }
+
+    //답변 수정
+    @Transactional
+    public void updateAnswer(Answer answer) {
+        answerRepository.save(answer);
+    }
+
+    //답변 삭제
+    @Transactional
+    public void deleteAnswer(Long id) {
+        answerRepository.deleteById(id);
+    }
 }
