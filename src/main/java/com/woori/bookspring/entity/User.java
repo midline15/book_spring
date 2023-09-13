f36eb8fc0f3ce87f81a578b9e0d6e69bc8288842
i <midline15@gmail.com>
2023-09-13 14:58:52 +0900

    userController

--- src/main/java/com/woori/bookspring/entity/User.java
+++ src/main/java/com/woori/bookspring/entity/User.java
@@ -3,12 +3,17 @@ package com.woori.bookspring.entity;
 import com.woori.bookspring.constant.OAuthType;
 import com.woori.bookspring.constant.Role;
 import com.woori.bookspring.dto.UserDto;
+import com.woori.bookspring.dto.UserUpdateDto;
 import com.woori.bookspring.entity.base.BaseEntity;
+import com.woori.bookspring.entity.ebook.Inventory;
+import com.woori.bookspring.entity.ebook.Like;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -42,9 +47,30 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private OAuthType oauth;
 
-    public UserDto of() {
-        return UserDto.builder()
-                .email(email)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orderList;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Inventory inventory;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Like like;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Cart cart;
+
+    public UserUpdateDto of() {
+        return UserUpdateDto.builder()
+                .id(id)
+                .name(name)
+                .address(address)
+                .birth(birth)
+                .phone(phone)
                 .build();
+    }
+
+    public void updateUser(UserUpdateDto userUpdateDto) {
+        name = userUpdateDto.getName();
+        birth = userUpdateDto.getBirth();
+        phone = userUpdateDto.getPhone();
+        address = userUpdateDto.getAddress();
     }
 }
